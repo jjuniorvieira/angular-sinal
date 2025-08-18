c008adbc6dfe91a055e8ca5b6b74ccbe31eae546
J. Junior <junior.jaava@gmail.com>
2025-08-18 21:57:30 +0200

    feat: add computed signal for total price calculation and update quantity on increment

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { signal } from '@angular/core';
+import { signal, computed } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +12,15 @@ export class AppComponent {
   title = 'my-app';
 
   counter = signal(0);
+  price = signal(100);
+  quantity = signal(2);
+  total = computed(() => this.price() * this.quantity());
+ 
 
   increment() {
     this.counter.update(value => value + 1); // increment by 1
+    this.quantity.update(value => value + 1); // increment quantity by 1
+     // Computed signal
+
   }
 }
