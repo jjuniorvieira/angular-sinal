a6c8887156f76329c93ad3e198c673e0b7e841e5
J. Junior <junior.jaava@gmail.com>
2025-10-12 14:47:27 +0200

    feat: implement OnInit lifecycle with observable example in AppComponent

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { signal, computed } from '@angular/core';
+import {filter, map, of} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,26 @@ import { signal, computed } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
 
+  title = 'my-app';
   counter = signal(0);
   price = signal(100);
   quantity = signal(2);
   total = computed(() => this.price() * this.quantity());
- 
+
+
+  ngOnInit(): void {
+      of(1, 2, 3, 4, 5) // create an observable
+        .pipe( // chains
+          filter(num => num % 2 === 0), // filter even numbers
+          map(num => num * 10) // multiply by 10
+        )
+        .subscribe(result => console.log(result)); // Output: 20, 40
+  }
+
+
+
 
   increment() {
     this.counter.update(value => value + 1); // increment by 1
