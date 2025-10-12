56558bf6c39eeef55447d47620d4f2eb4aee8542
J. Junior <junior.jaava@gmail.com>
2025-10-12 15:05:10 +0200

    feat: enhance AppComponent with HttpClient integration and mergeMap example
    - Added HttpClient and HttpClientModule for API calls.
    - Implemented mergeMap example using RxJS with user data retrieval.
    - Updated AppComponent to standalone component and imported necessary modules.

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { signal, computed } from '@angular/core';
-import {filter, map, of} from 'rxjs';
+import {filter, from, interval, map, mergeMap, of} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent implements OnInit {
 
@@ -17,14 +22,33 @@ export class AppComponent implements OnInit {
   quantity = signal(2);
   total = computed(() => this.price() * this.quantity());
 
+  constructor(private http: HttpClient) {
+    // console.log('interval and takeUntilDestroyed');
+    //
+    // interval(1000) // create an observable that emits every second
+    //   .pipe(takeUntilDestroyed()) // ensures the subscription stops when the component is destroyed; so no memory leaks
+    //   .subscribe(value => console.log(value));
+  }
+
 
   ngOnInit(): void {
-      of(1, 2, 3, 4, 5) // create an observable
-        .pipe( // chains
-          filter(num => num % 2 === 0), // filter even numbers
-          map(num => num * 10) // multiply by 10
-        )
-        .subscribe(result => console.log(result)); // Output: 20, 40
+    // console.log('filter and map');
+    //   of(1, 2, 3, 4, 5) // create an observable
+    //     .pipe( // chains
+    //       filter(num => num % 2 === 0), // filter even numbers
+    //       map(num => num * 10) // multiply by 10
+    //     )
+    //     .subscribe(result => console.log(result)); // Output: 20, 40
+
+    console.log('mergeMap');
+    const userIds = [1, 2, 3];
+    from(userIds)
+      .pipe(
+        mergeMap(id => this.http.get( // takes each emitted value and maps it to an observable, then merge all these inner observables into one
+          `https://jsonplaceholder.typicode.com/users/${id}`
+        ))
+      )
+      .subscribe(user => console.log(user));
   }
 
 
